86eb6d1a6d60130eef7129fe133e44ff2cc5f72c
BilankuluClyde <mpfunobilac@gmail.com>
2023-05-19 01:51:06 +0200

    Updated summary generator method with additional if statements to check for different possible paths

--- NumberSummary.java
+++ NumberSummary.java
@@ -44,7 +44,7 @@ public class NumberSummary implements NumberRangeSummarizer
      * in ranges, where as other non sequential numbers are comma deliminated.
      * <p>
      * This methods assumes that the provided input Collection of Integers does
-     * not contain duplicated and is sorted in ascending order.
+     * not contain duplicated integers and is sorted in ascending order.
      * @param input A comma separated list of Integers represented as a Collection
      * @return A comma separated list of Integers with grouped ranges of sequential numbers
      */
@@ -53,11 +53,45 @@ public class NumberSummary implements NumberRangeSummarizer
     // Method will receive collection : {1,3,6,7,8,12,13,14,15,21,22,23,24,31}
     {
         String summarizedStr = "Resutlt: ";
-        boolean sequential = false, withinRange = false;
+        boolean sequential = false, notInRange = true;
         Integer previous = ((ArrayList<Integer>)input).get(0);
         for(Integer number : input)
         {
-            summarizedStr += number.toString();
+            /* If we add 1 to the previous number and the previous number is still less than the current 
+             * number, then the previous number is obviously not sequential, thus we cant group it
+             * We then just need to add the previous number and a comma for the next number.
+             */
+            if(previous + 1 < number)
+            {
+                notInRange = true;
+                sequential = false;
+                summarizedStr = previous.toString() + ", ";
+                previous = number; // Set previous to the value of the current number before incrementing.
+            }
+            /* Else if the numebers are equal after adding 1 to the previous number then the numbers are 
+             * sequential 
+             */
+            else if (previous + 1 == number)
+            {
+                if (sequential)
+                {
+                    previous = number;
+                    continue;
+                }
+                else
+                {
+                    summarizedStr += previous + "-";
+                    sequential =  true;
+                    previous = number;
+                }
+
+            }
+            else if(previous == number && !sequential)
+            {
+                continue;
+            }
+            
+            summarizedStr += "o" + number.toString();
 
             summarizedStr += ", ";
         }
